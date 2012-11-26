6767ca4283c7229ab0503ae4aec667aa181135e5
michael79 <michael@barchy.be>
2012-11-26 22:31:13 +0100

    Reset

--- make.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-del BeamerCompanion.jar
-jikes -classpath "C:\Program Files\Java\jre6\lib\rt.jar";. BeamerCompanion.java
-7za a BeamerCompanion.jar BeamerCompanion.class BeamerCompanion.gif BeamerCompanion_*.properties NanoHTTPD*.class Base64.class META-INF
-pause
